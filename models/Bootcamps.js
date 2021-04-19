aed33c5a4ce2c596d649b098c583d3377d508983
HasanHasanbayli <hasanhasanbeyli@gmail.com>
2021-04-19 14:10:10 +0400

    first commit

---
+++ models/Bootcamps.js
@@ -0,0 +1,123 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
+const BootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name can not be more than 50 characters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters']
+    },
+    website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTPS'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+        ]
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+        // GeoJSON Point
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: 'lorem ipsum'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String
+    },
+    careers: {
+        // Array of strings
+        type: [String],
+        required: true,
+        enum: [
+            'Web Development',
+            'Mobile Development',
+            'UI/UX',
+            'Data Science',
+            'Business',
+            'Other'
+        ]
+    },
+    averageCost: Number,
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    housing: {
+        type: Boolean,
+        required: false
+    },
+    jobAssistance: {
+        type: Boolean,
+        default: false
+    },
+    jobGuarantee: {
+        type: Boolean,
+        default: false
+    },
+    acceptGi: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+// Geocode $ create location field
+BootcampSchema.pre('save', async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress:loc[0].formattedAddress,
+        street:loc[0].streetName,
+        street:loc[0].city,
+        street:loc[0].stateCode,
+        street:loc[0].zipcode,
+        street:loc[0].countrycode
+    };
+//Do not save address in DB
+    this.address=undefined;
+    next();
+})
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
